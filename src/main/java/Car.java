dc15f46dbd2a7615a2f24a0f2ac644bea06177d2
a.vybornov <a.vybornov@bft.ru>
2025-10-17 15:33:28 +0400

    create LessonFour

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    String model;
+    int year;
+
+
+    public void makeBeBe(){
+        System.out.println("Модель авто: " + model + " , " + year + " года");
+    }
+
+    public static void main(String[] args) {
+        Car tayote = new Car("RAV 4", 2022);
+        Car opel = new Car ("Astra", 2007);
+
+        tayote.makeBeBe();
+        opel.makeBeBe();
+    }
 }
